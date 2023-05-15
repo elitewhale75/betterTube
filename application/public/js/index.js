bda98d95202f40fb1e5be82da5296079edb2b7dc
elitewhale75 <realvignesh@gmail.com>
2023-05-14 23:38:01 -0700

    added counter for number of images on index.html. Finished assignment 4

--- application/public/js/index.js
+++ application/public/js/index.js
@@ -1,9 +1,12 @@
 var url = "https://jsonplaceholder.typicode.com/albums/2/photos";
+var imgCount;
 
 async function fetchWithStrings(){
     try{
         var response = await fetch(url); //Get URL
         var data = await response.json(); //Get JSON
+        imgCount = data.length;
+        display(imgCount)
         var htmlString = data.reduce(function(prev, obj) { 
             //Go through array and create string for HTML generation
             return prev + 
@@ -20,12 +23,24 @@ async function fetchWithStrings(){
         let x = document.getElementsByClassName('img-group');
         [...x].forEach(element => {
             element.addEventListener('click', (ev) =>{
-                console.log(ev.currentTarget);
+                delet(ev);
+                display(imgCount);
             });
         });
     }catch(error){
         console.log(error);
     }
+}
+
+// Function to Delete Elements
+function delet(ev){
+    ev.currentTarget.remove();
+    imgCount--;
+}
+
+// Function to Display Number of Images
+function display(counter){
+    document.getElementById('countdown').innerText = counter;
 }
 
 fetchWithStrings();
