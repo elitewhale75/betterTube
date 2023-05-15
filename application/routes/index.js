034106f1a9b716e6f6dca2ea8ad5f3fa0131634c
elitewhale75 <realvignesh@gmail.com>
2023-05-15 15:01:03 -0700

    Got node and the dependencies set up

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//localhost:3000
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+  res.render('index', { title: 'CSC 317 App', name:"Vignesh" });
 });
 
 module.exports = router;
