51710cbb4ed3e9d5e48f5c13202f31bb171aa0a1
elitewhale75 <realvignesh@gmail.com>
2023-05-15 21:17:02 -0700

    Converted HTML to Handelebar Templates

--- application/routes/index.js
+++ application/routes/index.js
@@ -4,7 +4,28 @@ var router = express.Router();
 /* GET home page. */
 //localhost:3000
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Vignesh" });
+  res.render('index', { title: 'Home', name:"Vignesh" });
 });
+
+router.get("/login", function(req,res){
+  res.render('login', { title: 'Login'});
+})
+
+router.get("/registration", function(req,res){
+  res.render('registration', { title: "Registration Form", script: 'public/js/validation.js'});
+})
+
+router.get("/postvideo", function(req,res){
+  res.render('postvideo', { title: 'Post Video'});
+})
+
+router.get("/profile", function(req,res){
+  res.render('profile' , { title: 'User Profile'});
+})  
+
+//Make Title the name of the video  
+router.get("/viewpost/:id(\\d+)", function(req,res){
+  res.render('viewpost' , { title: 'View Post'});
+})
 
 module.exports = router;
