498b168ce66ac6d61b16326acf16742f34c2568e
elitewhale75 <realvignesh@gmail.com>
2023-05-16 15:40:50 -0700

    connected JavaScript and Database

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../conf/database');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* GET localhost:3000/users */
+router.get('/', async function(req, res, next) { 
+  try{
+    let [rows, fields] = await db.query(`select * from users;`);
+    res.status(200).json({rows, fields});
+  }catch(error){
+    next(error);
+  }
 });
 
 module.exports = router;
