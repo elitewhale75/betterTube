36054f85f370c8fefb1e502a53fdbf47fde840fd
elitewhale75 <realvignesh@gmail.com>
2023-05-17 18:13:33 -0700

    able to create thumbnails

---
+++ application/middleware/posts.js
@@ -0,0 +1,24 @@
+var pathToFFMPEG = require('ffmpeg-static');
+var exec = require('child_process').exec;
+
+module.exports = {
+    makeThumbnail: function (req, res, next) {
+        if (!req.file) {
+            next(new Error('File upload failed'));
+        } else {
+            try {
+                var destinationOfThumbnail = `public/images/uploads/thumbnail-${
+                    req.file.filename.split(".")[0]}.png`
+                    console.log(pathToFFMPEG);
+                var thumbnailCommand = `${pathToFFMPEG} -i ${req.
+                    file.path} -ss 00:00:01 -vframes 1 ${destinationOfThumbnail}`;
+                exec(thumbnailCommand);
+                req.file.thumbnail = destinationOfThumbnail;
+                next();
+            } catch (error) {
+                next(error);
+            }
+
+        }
+    }
+}
--- application/routes/posts.js
+++ application/routes/posts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+const { makeThumbnail } = require('../middleware/posts');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -9,13 +10,13 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         var fileExt = file.mimetype.split("/")[1];
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, `${file.fieldname}- ${uniqueSuffix}.${fileExt}`)
+        cb(null, `${file.fieldname}-${uniqueSuffix}.${fileExt}`)
     }
 })
 
 const upload = multer({ storage: storage })
 
-router.post("/create", upload.single("post_video"), function (req, res, next) {
+router.post("/create", upload.single("post_video"), makeThumbnail, function (req, res, next) {
     console.log(req.file);
     console.group(req.body);
     res.end();
